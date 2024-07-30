5bb2aee05fd944c5d0ec6952a121c1a6a0752c47
Sangdeveloper7 <leesanghyun9757@gmail.com>
2024-07-30 21:22:21 +0900

    first commit message

---
+++ src/main/java/com/example/me_one/MeOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.me_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeOneApplication.class, args);
+    }
+
+}
