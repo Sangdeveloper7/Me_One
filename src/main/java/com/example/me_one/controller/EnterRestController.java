1fbbc23f1d667ed1482c2b07ce6eb345be5852d3
Sangdeveloper7 <leesanghyun9757@gmail.com>
2024-07-31 23:40:03 +0900

    7/31

--- src/main/java/com/example/me_one/controller/EnterRestController.java
+++ src/main/java/com/example/me_one/controller/EnterRestController.java
@@ -1,4 +1,36 @@
 package com.example.me_one.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/api/tbboard")
+@RestController
 public class EnterRestController {
+
+    EnterRestController enterRestController;
+
+    public EnterRestController(
+            EnterRestController enterRestController
+    ){
+        this.enterRestController = enterRestController;
+    }
+
+    @GetMapping("/create")
+    public Map<String, Object> create(
+            @RequestParam String title
+            , @RequestParam String content
+            , @RequestParam String writer
+    ){
+        Map<String, Object> tbboard = new HashMap<>();
+        tbboard.put("title", title);
+        tbboard.put("content", content);
+        tbboard.put("writer", writer);
+        return enterService.create(enterService);
+    }
 }
--- src/main/java/com/example/me_one/controller/LoginRestController.java
+++ src/main/java/com/example/me_one/controller/LoginRestController.java
@@ -1,4 +1,49 @@
 package com.example.me_one.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginRestController {
+
+    @GetMapping("/login")
+    public boolean login(@RequestParam String id, @RequestParam String pw){
+        boolean result = false;
+        System.out.println("id : " + id + "// pw" + pw);
+        if("admin".equals(id)){
+            if("abcd1234".equals(pw)){
+                result = true;
+            }
+        }
+
+        return result;
+    }
+
+    @GetMapping("/login2")
+    public Map<String, Object> login2(@RequestParam Map<String, Object> params){
+        Map<String, Object> returnData = new HashMap<String, Object>();
+        boolean result = false;
+        String msg = "";
+        String id = (String) params.get("id");
+        String pw = (String) params.get("pw");
+        System.out.println("id : " + id + "// pw" + pw);
+        if("admin".equals(id)){
+            if("abcd1234".equals(pw)){
+                result = true;
+                msg = "정상 로그인 완료!";
+            } else {
+                msg = "pw 모르면 ㄴㄴ!!";
+            }
+        } else {
+            msg = "id도 모르니?";
+        }
+
+        returnData.put("result", result);
+        returnData.put("msg", msg);
+
+
+        return returnData;
+    }
 }
